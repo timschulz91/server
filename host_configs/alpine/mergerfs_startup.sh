57b223c872f09759854a7661fe20275e0c811959
timschulz91 <timschulz91@gmail.com>
2017-11-25 20:51:14 -0800

    added workaround to mount mergerfs on boot

---
+++ host_configs/alpine/mergerfs_startup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# get the local service to start
+rc-update add local default
+
+# add the mount script
+touch /etc/local.d/mount_mergerfs.start
+echo "mount -a" >> /etc/local.d/mount_mergerfs.start
+chmod + x /etc/local.d/mount_mergerfs.start
