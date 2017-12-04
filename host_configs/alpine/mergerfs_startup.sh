b5e118c3a88f9652c52feb5a5a97134ec08bd9d2
timschulz91 <timschulz91@gmail.com>
2017-12-03 23:13:13 -0800

    typo

--- host_configs/alpine/mergerfs_startup.sh
+++ host_configs/alpine/mergerfs_startup.sh
@@ -6,4 +6,4 @@ rc-update add local default
 # add the mount script
 touch /etc/local.d/mount_mergerfs.start
 echo "mount -a" >> /etc/local.d/mount_mergerfs.start
-chmod + x /etc/local.d/mount_mergerfs.start
+chmod +x /etc/local.d/mount_mergerfs.start
